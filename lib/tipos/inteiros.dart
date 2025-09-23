a32daa55fad14bdf0e41bf6fb9d516dfa77b7e8e
guilherme9822 <josegueedes12@hotmail.com>
2025-09-23 12:23:02 -0300

    Aula de inteiros.

---
+++ lib/tipos/inteiros.dart
@@ -0,0 +1,17 @@
+void main() {
+  int altura = 178;
+
+  print("Altura: $altura cm");
+
+  int ano = 2025;
+
+  print("Ano: $ano");
+
+  int idade = 24;
+
+  print("Idade: $idade anos");
+
+  int anoNascimento = ano - idade;
+
+  print("Ano de Nascimento: $anoNascimento");
+}

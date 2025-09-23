e8cfc1eeb4a9436e565945ec29d0a5083c42c659
guilherme9822 <josegueedes12@hotmail.com>
2025-09-23 11:41:35 -0300

    Aula Variaveis

---
+++ lib/tipos/variaveis.dart
@@ -0,0 +1,39 @@
+void main() {
+  final int altura;
+
+  altura = 80;
+
+  print(altura);
+
+  var nome = "Paulo";
+
+  nome = "Paulo Fernando";
+
+  print(nome);
+
+  var idade = 24;
+
+  print(idade);
+
+  var peso = 102;
+
+  peso = 90;
+
+  print(peso);
+
+  var fazAcademia = true;
+
+  fazAcademia = false;
+
+  print(fazAcademia);
+
+  const nomeCompleto = "Jose Guilherme Guedes de Oliveira";
+
+  print(nomeCompleto);
+
+  late String nomePai;
+
+  nomePai = "FUlano";
+
+  print(nomePai);
+}

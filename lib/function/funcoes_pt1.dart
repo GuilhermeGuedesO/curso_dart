6d8a788a46982f952bda0c401ea9734ce64e5fd1
guilherme9822 <josegueedes12@hotmail.com>
2025-09-24 09:58:23 -0300

    Aula funçoes parte 1

---
+++ lib/function/funcoes_pt1.dart
@@ -0,0 +1,21 @@
+int somar(int number1, int number2) {
+  return number1 + number2;
+}
+
+double dividir(int numb1, int numb2) {
+  return numb1 / numb2;
+}
+
+int multiplicar(int numb1, int numb2) {
+  return numb1 * numb2;
+}
+
+void main() {
+  print("Valor da soma: ${somar(1, 6)}");
+
+  print("Valor da soma: ${somar(94375, 13241)}");
+
+  print("Valor da divisao: ${dividir(100, 400)}");
+
+  print("Valor da multiplicaçao: ${multiplicar(5, 5)}");
+}

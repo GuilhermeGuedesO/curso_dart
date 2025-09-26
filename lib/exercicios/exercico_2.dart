1888d77c9f8107ad01258f5beff7671982ec2a73
guilherme9822 <josegueedes12@hotmail.com>
2025-09-26 11:34:30 -0300

    Exercio 2.

--- lib/exercicios/exercico_2.dart
+++ lib/exercicios/exercico_2.dart
@@ -2,3 +2,20 @@
  forem iguais, deverá somar os dois valores,caso contrário devera multiplicar A por B.
 Ao final de qualquer um dos cálculos deve-se atribuir o resultado a uma variável 
 C e imprimir seu valor na tela.*/
+
+void main() {
+  int a = 4;
+  int b = 3;
+  int c;
+
+  if (a == b) {
+    c = a + b;
+    print("Os valores de A e B sao iguais o resultado da soma é $c.");
+  }
+  if (a != b) {
+    c = a * b;
+    print(
+      "Os valores de A e B sao diferentes o resultado da multiplicaçao é $c.",
+    );
+  }
+}

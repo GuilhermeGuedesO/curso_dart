94c35c060ed9cbfda5ddee55fbce7a1abb1d1213
guilherme9822 <josegueedes12@hotmail.com>
2025-09-30 13:30:11 -0300

    Aula de Extesions.

---
+++ lib/poo/extensions.dart
@@ -0,0 +1,51 @@
+extension IntExtension on int {
+  int quadrado() {
+    return this * this;
+  }
+
+  bool ehPar() {
+    return this % 2 == 0;
+  }
+
+  bool ehImpar() {
+    return !ehPar();
+  }
+}
+
+extension DateExtension on DateTime {
+  String paraDataBrasileira() {
+    final dia = day.toString().padLeft(2, "0");
+    final mes = month.toString().padLeft(2, "0");
+    final ano = year;
+
+    return "$dia/$mes/$ano";
+  }
+
+  String paraHoraBrasileira() {
+    final hora = hour.toString().padLeft(2, "0");
+    final minuto = minute.toString().padLeft(2, "0");
+
+    return "$hora:$minuto";
+  }
+}
+
+void main() {
+  final numero = 4;
+
+  print(numero.quadrado());
+
+  print("Par: ${numero.ehPar()}");
+
+  print("Impar: ${numero.ehImpar()}");
+
+  final dataAtual = DateTime.now();
+
+  print(dataAtual.paraDataBrasileira());
+
+  print(dataAtual.paraHoraBrasileira());
+
+  final dataEHoraAtual =
+      "${dataAtual.paraDataBrasileira()} ás ${dataAtual.paraHoraBrasileira()}";
+
+  print(dataEHoraAtual);
+}
